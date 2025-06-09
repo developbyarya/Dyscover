38475d096ac48f05839f2af6f4788ddff6cbd57c
Maan-py <mluqmaan22@gmail.com>
2025-06-09 18:38:52 +0700

    chore: update splash image, gitignore, and text component

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,15 +1,15 @@
-import { Text as RNText, TextProps } from 'react-native';
+import { Text as RNText, TextProps } from "react-native";
 
 export function Text({ style, ...props }: TextProps) {
   return (
     <RNText
       style={[
         {
-          fontFamily: 'Monstserrat',
+          fontFamily: "Monstserrat",
         },
         style,
       ]}
       {...props}
     />
   );
-} 
+}
